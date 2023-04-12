7b1821b6009badc6fd01fe26cd736d927f44dfd8
sotiris13 <s.pantioras@gmail.com>
2023-04-12 07:11:06 +0100

    synchronise db true

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -45,7 +45,7 @@ export const config: VendureConfig = {
         type: 'postgres',
         // See the README.md "Migrations" section for an explanation of
         // the `synchronize` and `migrations` options.
-        synchronize: false,
+        synchronize: true,
         migrations: [path.join(__dirname, './migrations/*.+(ts|js)')],
         logging: false,
         database: process.env.DB_NAME,
